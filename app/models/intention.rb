03f11a5c8d1c50d9d196cc7b4ff606933dcdf61d
Brian Becker <brian@eventmeadow.com>
2015-03-30 22:23:53 -0700

    updated intention form

--- app/models/intention.rb
+++ app/models/intention.rb
@@ -8,7 +8,7 @@ class Intention < ActiveRecord::Base
 
   enum status: { going_has_ticket: 1, going_needs_ticket: 2, not_going_has_ticket: 3, not_going_no_ticket: 4 }
   enum lodging: { tent: 1, yurt: 2, car: 3, other_lodging: 4 }
-  enum transportation: { driving: 1, carpool: 2, bus: 3, walk: 4, other_transportation: 5 }
+  enum transportation: { driving: 1, riding_with_someone: 2, bus: 3, walk: 4, other_transportation: 5 }
 
   def Intention.for_next_event
   	where(event: Event.next_event)
