67102bc17b797f8b07f26f26d50bf0dedc297ac9
Brian Becker <becker1701@gmail.com>
2017-04-12 17:03:55 -0700

    updated password reset mailer and email host configuration

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,29 +3,29 @@ class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
   before_action :expired_token, only: [:edit, :udpate]
-  
+
   def new
   end
 
   def create
-  	
-    # 
-  	@user = User.find_by(email: params[:password_reset][:email].downcase) if params[:password_reset][:email]
-  	
-  	if @user && !@user.activated?
+
+    #
+    @user = User.find_by(email: params[:password_reset][:email].downcase) if params[:password_reset][:email]
+
+    if @user && !@user.activated?
       # @user.send_activation_email
       flash[:warning] = "Your account is not activated. Check your email for the activation link."
       redirect_to root_url
 
     elsif @user && @user.activated?
-    	@user.reset_digest_attributes(:password_reset)
-  		flash[:success] = "A password reset email has been sent."
-		  redirect_to login_url
-    
+      @user.reset_digest_attributes(:password_reset)
+      flash[:success] = "A password reset email has been sent."
+      redirect_to login_url
+
     else
       flash.now[:danger] = "Email address not found."
-		  render :new
-  	end
+      render :new
+    end
 
   end
 
@@ -35,19 +35,19 @@ class PasswordResetsController < ApplicationController
 
 
   def update
-    # 
+    #
     if password_blank?
       # flash.now[:danger] = "Password can't be blank."
 
       @user.blank_password_reset_error
       render :edit
-    elsif @user.update_attributes(user_params)  
+    elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "Password successfully reset"
       redirect_to @user
     else
       render :edit
-    end      
+    end
   end
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'flirtcampsf.herokuapp.com'
+  # host = 'flirtcampsf.herokuapp.com'
+  host = 'flirtcampsf.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
